ad0bc9e4c38659b973683610aad84a9efa0608fe
Kyle Lobban <kyle.lobban@kik.com>
2014-09-12 12:14:36 -0400

    Added support for created_gte and created_lte for issues.

--- index.js
+++ index.js
@@ -186,6 +186,16 @@ Crashlytics.prototype.issues = function(app, options, callback) {
 		options.build_equals = options.build;
 	}
 
+	if (options.start) {
+		options.created_gte = options.start;
+	}
+
+	if (options.end) {
+		options.created_lte = options.end;
+	} else {
+		options.created_lte = ~~(+new Date()/1000);
+	}
+
 	return this._apiCall(app, '/issues.json?'+encodeParamters(options), {}, null, processResult);
 };
 
